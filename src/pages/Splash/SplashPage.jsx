26b7c488e4e76c53dec99ca26c1f29f80e3153bd
Aliyu Aminu <aliyu.aminu@pwc.com>
2026-08-12 12:45:30 +0100

    Spash screen

--- src/pages/Splash/SplashPage.jsx
+++ src/pages/Splash/SplashPage.jsx
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import MobileLayout from "../../components/layout/MobileLayout";
-import BottomNavigation from "../../components/layout/BottomNavigation";
+import Logo from '../../assets/images/Logo.png';
 
 const SplashPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("/login");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <MobileLayout>
-      <div className="p-5">
-        <h1 className="text-3xl font-bold">Splash Screen</h1>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-black px-6">
+        <h1>
+            <img src={Logo} alt="cryptovault" className="mt-6 text-[#00E58F] text-4xl font-bold" />
+        </h1>
       </div>
-
-      <BottomNavigation />
     </MobileLayout>
   );
 };
